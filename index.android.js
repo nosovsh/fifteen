f2c9c735de3e21f232efc301516340fa42121f5a
trashgenerator <trashgenerator@gmail.com>
2015-10-15 22:42:03 +0300

    basic markup

--- index.android.js
+++ index.android.js
@@ -12,20 +12,12 @@ var {
   View,
 } = React;
 
+var Game = require('./src/Game');
+
 var Fifteen = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Game/>
     );
   }
 });
---
+++ src/Game.js
@@ -0,0 +1,76 @@
+var React = require('react-native');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Animated,
+  PanResponder,
+  Text,
+  ScrollView,
+  addons,
+  TouchableOpacity
+  } = React;
+
+
+var Game = React.createClass({
+  render() {
+    return (
+      <View  style={styles.container}>
+
+        <View style={styles.headerArea}>
+          <Text style={styles.header}>The fifteen game</Text>
+          <Text style={styles.header2}>Slide tiles to put them in order.</Text>
+        </View>
+
+        <View style={styles.mainArea}>
+        </View>
+
+        <View style={styles.bottomArea}>
+          <TouchableOpacity onPress={this.newGame}>
+            <Text style={styles.help}>Give up?</Text>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F4F4F4',
+  },
+  headerArea: {
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainArea: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomArea: {
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 25,
+    fontWeight: '200',
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  header2: {
+    fontSize: 15,
+    fontWeight: '200',
+    textAlign: 'center',
+  },
+  help: {
+    opacity: 0.7,
+  }
+});
+
+module.exports = Game;
